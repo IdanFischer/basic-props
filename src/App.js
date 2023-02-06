95247ef84174f2c1da5f0894654a9ce9f1bc5f52
Idan Fischer <Idanfischer5@gmail.com>
2023-02-06 11:02:49 -0500

    handling objects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import Greeting from './components/Greeting';
 import './App.css';
+import StudentInfo from './components/StudentInfo';
+
+const student1 = {
+  firstName: "Gisselle",
+  lastName: "Pombar",
+  email: "gigi@bocacode.com",
+  age: 26
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is Props!</h1>
-        <Greeting firstName="Gisselle" lastName="Pombar" emoji="🤗"/>
-        <Greeting firstName="Gisele" lastName="Abzalov" emoji="😘"/>
-        <Greeting firstName="Idan" lastName="Fischer"/>
+        <Greeting firstName="Idan" lastName="Fischer" emoji="😈" />
+        <StudentInfo
+        student={
+          {firstName: "Idan",
+          lastName: "Fischer",
+          email: "IdanFischer@bocacode.com",
+          age: 18
+        }}
+        />
+        <Greeting firstName="Gisselle" lastName="Pombar" emoji="🤗" />
+        <StudentInfo student={student1} />
       </header>
     </div>
   );
